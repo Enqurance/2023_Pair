762e89c70926fc3bc36f0063c5c6cc088aed8976
Enqurance <20373980@buaa.edu.cn>
2023-03-08 10:24:50 +0800

    Add GUI

--- GUI/src/Main.java
+++ GUI/src/Main.java
@@ -5,33 +5,36 @@ import javax.swing.*;
 
 public class Main {
     public static void main(String[] args) {
-        new MyFrame();
+        new MyFrame().setVisible(true);
     }
 }
 
 class MyFrame extends JFrame {
     public MyFrame() {
         setTitle("Longest-Word-Chain");
-        setSize(1000, 800);
+        setSize(800, 600);
+        setLocation(300, 200);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        Container c = getContentPane();
-        setVisible(true);       // 设置窗口可见性
-        setLayout(new BorderLayout());
+        setResizable(false);
+        setFrameLayout();
+    }
 
+    private void setFrameLayout() {
         JPanel panel1 = new JPanel();
-        panel1.setBackground(Color.CYAN);
-        panel1.setSize();
-        JButton button2 = new JButton("左·写菜单的比较多，但不一定用");
-        JButton button3 = new JButton("中·表格信息");
-        JButton button4 = new JButton("右·很少用到");
-        JButton button5 = new JButton("下·备案号啥的");
-        //上n下s左w右e中center
-        add(panel1, BorderLayout.NORTH);
-        add(button2, BorderLayout.WEST);
-        add(button3, BorderLayout.CENTER);
-        add(button4, BorderLayout.EAST);
-        add(button5, BorderLayout.SOUTH);
-        setBounds(300, 200, 600, 300);
+        panel1.setSize(500, 300);
+        add(panel1, BorderLayout.WEST);
+
+        JPanel panel2 = new JPanel();
+        panel2.setLocation(0, 300);
+        panel2.setSize(500, 300);
+        add(panel2, BorderLayout.WEST);
+
+        JPanel panel3 = new JPanel();
+        panel3.setLocation(500, 0);
+        panel3.setSize(300, 600);
+        add(panel3, BorderLayout.EAST);
+
+        JTextArea jta = new JTextArea("这是一段文本域");
     }
 
 }
