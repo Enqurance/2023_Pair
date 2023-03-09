5acb38ae13df40a89002e389a985928c72f3f4df
zyl <example@user.com>
2023-03-09 17:47:46 +0800

    update CMakeLists

--- cmake-build-debug/CMakeFiles/Wordlist.dir/cmake_clean.cmake
+++ cmake-build-debug/CMakeFiles/Wordlist.dir/cmake_clean.cmake
@@ -1,9 +1,9 @@
 file(REMOVE_RECURSE
   "CMakeFiles/Wordlist.dir/src/main.cpp.obj"
   "CMakeFiles/Wordlist.dir/src/main.cpp.obj.d"
-  "Wordlist.exe"
-  "Wordlist.exe.manifest"
-  "Wordlist.pdb"
+  "D:/BUAA_Files/Learning_Files/Senior_Three_2/BUAA_SE/Pair_Project/2023_Pair/bin/Wordlist.exe"
+  "D:/BUAA_Files/Learning_Files/Senior_Three_2/BUAA_SE/Pair_Project/2023_Pair/bin/Wordlist.exe.manifest"
+  "D:/BUAA_Files/Learning_Files/Senior_Three_2/BUAA_SE/Pair_Project/2023_Pair/bin/Wordlist.pdb"
   "libWordlist.dll.a"
 )
 
