ea0366dd32508fb9b001ebb4eb91c3395844216e
Enqurance <20373980@buaa.edu.cn>
2023-03-10 22:03:50 +0800

    Ready to do unit test

--- src/Test.cpp
+++ src/Test.cpp
@@ -1,8 +1,9 @@
 #include "FileReader.h"
+/* 当前文件执行在bin目录下 */
 
 int main() {
     FileReader f;
-    f.read_file("../Testfile.txt");
+    f.read_file("../src/Testfile1.txt");
     f.print_words();
 }
 
