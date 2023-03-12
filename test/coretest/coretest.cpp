7ec1cc24d90550ad57469a5e5a6fb96f25c834f4
Enqurance <20373980@buaa.edu.cn>
2023-03-12 20:34:35 +0800

    Found bug while doing test

--- test/coretest/coretest.cpp
+++ test/coretest/coretest.cpp
@@ -13,7 +13,7 @@ protected:
     }
 };
 
-// 测试用例1
+// -n
 TEST_F(CoreTest, Test_N) {
     int words_size;
     FileIO f;
@@ -29,7 +29,7 @@ TEST_F(CoreTest, Test_N) {
     EXPECT_EQ(f.output_screen(result), 1);
 }
 
-//测试用例2
+// -w
 TEST_F(CoreTest, Test_W) {
     int words_size;
     FileIO f;
@@ -46,6 +46,7 @@ TEST_F(CoreTest, Test_W) {
     EXPECT_EQ(f.output_file(result), 1);    // 测试时，solution.txt在bin下
 }
 
+// -h -w
 TEST_F(CoreTest, Test_H_W) {
     int words_size;
     FileIO f;
@@ -62,6 +63,7 @@ TEST_F(CoreTest, Test_H_W) {
     EXPECT_EQ(f.output_file(result), 1);    // 测试时，solution.txt在bin下
 }
 
+// -t -w
 TEST_F(CoreTest, Test_T_W) {
     int words_size;
     FileIO f;
@@ -78,6 +80,59 @@ TEST_F(CoreTest, Test_T_W) {
     EXPECT_EQ(f.output_file(result), 1);    // 测试时，solution.txt在bin下
 }
 
+// -j -w
+TEST_F(CoreTest, Test_J_W) {
+    int words_size;
+    FileIO f;
+    string filename = "../test/Testfiles/input2.txt";
+    EXPECT_EQ(f.read_file(filename), 1);
+    vector<string> words = f.get_words(words_size);
+    ASSERT_EQ(words_size, 11);
+
+    /* 准备工作完成，开始测试 */
+    Core core = *new Core(words, words_size);
+    vector<string> result;
+    char c = 0;
+    gen_chain_word(words, words_size, result, c, c, 'a', false);
+    EXPECT_EQ(f.output_file(result), 1);    // 测试时，solution.txt在bin下
+}
+
+
+// -h -t -w
+TEST_F(CoreTest, Test_H_T_W) {
+    int words_size;
+    FileIO f;
+    string filename = "../test/Testfiles/input2.txt";
+    EXPECT_EQ(f.read_file(filename), 1);
+    vector<string> words = f.get_words(words_size);
+    ASSERT_EQ(words_size, 11);
+
+    /* 准备工作完成，开始测试 */
+    Core core = *new Core(words, words_size);
+    vector<string> result;
+    char c = 0;
+    gen_chain_word(words, words_size, result, 'a', 't', c, false);
+    EXPECT_EQ(f.output_file(result), 1);    // 测试时，solution.txt在bin下
+}
+
+// -h -t -j -w
+TEST_F(CoreTest, Test_H_T_J_W) {
+    int words_size;
+    FileIO f;
+    string filename = "../test/Testfiles/input2.txt";
+    EXPECT_EQ(f.read_file(filename), 1);
+    vector<string> words = f.get_words(words_size);
+    ASSERT_EQ(words_size, 11);
+
+    /* 准备工作完成，开始测试 */
+    Core core = *new Core(words, words_size);
+    vector<string> result;
+    char c = 0;
+    gen_chain_word(words, words_size, result, 'a', 't', 'a', false);
+    EXPECT_EQ(f.output_file(result), 1);    // 测试时，solution.txt在bin下
+}
+
+// -c
 TEST_F(CoreTest, Test_C) {
     int words_size;
     FileIO f;
@@ -91,6 +146,73 @@ TEST_F(CoreTest, Test_C) {
     vector<string> result;
     char c = 0;
     gen_chain_char(words, words_size, result, c, c, c, false);
+    EXPECT_EQ(f.output_file(result), 1);    // 测试时，solution.txt在bin下
+}
+
+// -h -c
+TEST_F(CoreTest, Test_H_C) {
+    int words_size;
+    FileIO f;
+    string filename = "../test/Testfiles/input2.txt";
+    EXPECT_EQ(f.read_file(filename), 1);
+    vector<string> words = f.get_words(words_size);
+    ASSERT_EQ(words_size, 11);
+
+    /* 准备工作完成，开始测试 */
+    Core core = *new Core(words, words_size);
+    vector<string> result;
+    char c = 0;
+    gen_chain_char(words, words_size, result, 'a', c, c, false);
+    EXPECT_EQ(f.output_file(result), 1);    // 测试时，solution.txt在bin下
+}
+
+// -t -c
+TEST_F(CoreTest, Test_T_C) {
+    int words_size;
+    FileIO f;
+    string filename = "../test/Testfiles/input2.txt";
+    EXPECT_EQ(f.read_file(filename), 1);
+    vector<string> words = f.get_words(words_size);
+    ASSERT_EQ(words_size, 11);
+
+    /* 准备工作完成，开始测试 */
+    Core core = *new Core(words, words_size);
+    vector<string> result;
+    char c = 0;
+    gen_chain_char(words, words_size, result, c, 't', c, false);
+    EXPECT_EQ(f.output_file(result), 1);    // 测试时，solution.txt在bin下
+}
+
+// -h -t -c
+TEST_F(CoreTest, Test_H_T_C) {
+    int words_size;
+    FileIO f;
+    string filename = "../test/Testfiles/input2.txt";
+    EXPECT_EQ(f.read_file(filename), 1);
+    vector<string> words = f.get_words(words_size);
+    ASSERT_EQ(words_size, 11);
+
+    /* 准备工作完成，开始测试 */
+    Core core = *new Core(words, words_size);
+    vector<string> result;
+    char c = 0;
+    gen_chain_char(words, words_size, result, 'a', 'e', c, false);
+    EXPECT_EQ(f.output_file(result), 1);    // 测试时，solution.txt在bin下
+}
+
+// -h -t -j -c
+TEST_F(CoreTest, Test_H_T_R_C) {
+    int words_size;
+    FileIO f;
+    string filename = "../test/Testfiles/input2.txt";
+    EXPECT_EQ(f.read_file(filename), 1);
+    vector<string> words = f.get_words(words_size);
+    ASSERT_EQ(words_size, 11);
+
+    /* 准备工作完成，开始测试 */
+    Core core = *new Core(words, words_size);
+    vector<string> result;
+    gen_chain_char(words, words_size, result, 'a', 'e', 'a', false);
     EXPECT_EQ(f.output_file(result), 1);    // 测试时，solution.txt在bin下
 }
 
