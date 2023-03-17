4d3c0546d961b098af76b8e86452151136ffc24d
Enqurance <1581294964@QQ.com>
2023-03-17 10:42:37 +0800

    Doing perf test

---
+++ src/PerfFileIO.cpp
@@ -0,0 +1,5 @@
+//
+// Created by linzi on 2023/3/16.
+//
+
+#include "PerfFileIO.h"
---
+++ test/PerfTest/PerfTest.cpp
@@ -0,0 +1,51 @@
+#include <gtest/gtest.h>
+#include "string"
+#include "../../src/PerfFileIO.h"
+#include "../../src/PerfCore.h"
+
+using namespace std;
+
+void PerfTestOne();
+
+void PerfTestTwo();
+
+void PerfTestThree();
+
+int main() {
+    PerfTestOne();
+    PerfTestTwo();
+    PerfTestThree();
+}
+
+// most words
+void PerfTestOne() {
+    string filename = "../test/Testfiles/PTest1.txt";
+    vector<string> words;
+    vector<vector<string>> result;
+    FileIO f = FileIO::getInstance();
+    f.read_file(filename);
+    int len = f.get_words(words);
+    Core core = *new Core(words, len);
+    core.genAllWordChain(result);
+    FileIO::output_screen(result);
+}
+
+void PerfTestTwo() {
+
+}
+
+// most chars
+void PerfTestThree() {
+    string filename = "../test/Testfiles/PTest3.txt";
+    vector<string> words;
+    char c = 0;
+    vector<string> result;
+    FileIO f = FileIO::getInstance();
+    f.read_file(filename);
+    int len = f.get_words(words);
+    Core core = *new Core(words, len, true, c, c, c, true);
+    core.genMaxWordCountChain(result);
+    FileIO::output_file(result);
+}
+
+
