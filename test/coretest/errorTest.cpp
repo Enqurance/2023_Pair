08b922172e4f0f7c848c3e857c9c9bdbb99b4b4f
Enqurance <1581294964@QQ.com>
2023-03-19 17:44:20 +0800

    Added error test

--- test/coretest/errorTest.cpp
+++ test/coretest/errorTest.cpp
@@ -2,7 +2,7 @@
 #include <gtest/gtest.h>
 #include "string"
 #include "windows.h"
-#include "../../src/PerfFileIO.h"
+#include "../../src/FileIO.h"
 #include "../../src/Core.h"
 
 // 测试套件示例
@@ -47,6 +47,13 @@ TEST_F(CoreTest, Test_NO_FILE) {
     ASSERT_EQ(f.read_file(filename), -1);
 }
 
+// no file
+TEST_F(CoreTest, Test_ILLEGAL_FILE) {
+    FileIO f = FileIO::getInstance();
+    string filename = "../test/Testfiles/MDFile.md";
+    ASSERT_EQ(f.read_file(filename), -1);
+}
+
 // illegal loop
 TEST_F(CoreTest, Test_ILLEGAL_LOOP) {
     FileIO f = FileIO::getInstance();
@@ -59,4 +66,24 @@ TEST_F(CoreTest, Test_ILLEGAL_LOOP) {
     /* 开始测试 */
     vector<vector<string>> result;
     ASSERT_EQ(gen_chains_all(words, size, result), -1);
+}
+
+// parameter error
+TEST_F(CoreTest, Test_PARAMETER_ERROR) {
+    int ret1 = std::system("../../bin/Wordlist.exe -c -w ../Testfiles/input1.txt");
+    ASSERT_EQ(ret1, 11);
+    int ret2 = std::system("../../bin/Wordlist.exe ../Testfiles/input1.txt");
+    ASSERT_EQ(ret2, 12);
+    int ret3 = std::system("../../bin/Wordlist.exe -w -h ../Testfiles/input1.txt");
+    ASSERT_EQ(ret3, 8);
+    int ret4 = std::system("../../bin/Wordlist.exe -w -t bb ../Testfiles/input1.txt");
+    ASSERT_EQ(ret4, 9);
+    int ret5 = std::system("../../bin/Wordlist.exe -w -j 8 ../Testfiles/input1.txt");
+    ASSERT_EQ(ret5, 10);
+    int ret6 = std::system("../../bin/Wordlist.exe -n -h a ../Testfiles/input1.txt");
+    ASSERT_EQ(ret6, 7);
+    int ret7 = std::system("../../bin/Wordlist.exe -n -o ../Testfiles/input1.txt");
+    ASSERT_EQ(ret7, 5);
+    int ret8 = std::system("../../bin/Wordlist.exe -n -h a -h a ../Testfiles/input1.txt");
+    ASSERT_EQ(ret8, 6);
 }
